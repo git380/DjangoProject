4044f8b2583d00f99f406ab6dcaf71ec18778ba6
utsumi <201010@std.hi-joho.ac.jp>
2024-05-13 16:07:08 +0900

    コミット

--- プロジェクト名/settings.py
+++ プロジェクト名/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'アプリケーション名.apps.アプリケーション名Config',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'プロジェクト名.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
